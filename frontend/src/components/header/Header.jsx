1d4024fc374ba6d2d345fd1271910a832716f90b
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-16 21:11:03 +0300

    feat - add header search bar

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -6,6 +6,8 @@ import {
   faPlane,
   faCar,
   faTaxi,
+  faCalendarDays,
+  faPerson,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
@@ -34,9 +36,30 @@ const Header = () => {
             <span>taxis</span>
           </div>
         </div>
-      <h1 className="headerTitle">A lifetime of discounts? It's a Genius.</h1>
-      <p className="headerSubTitle">Get rewarded for your travels - unlock instant savings of 10% or more with a free booking account</p>
-      <button className="headerBtn">Sign in / Register</button>
+        <h1 className="headerTitle">A lifetime of discounts? It's a Genius.</h1>
+        <p className="headerSubTitle">
+          Get rewarded for your travels - unlock instant savings of 10% or more
+          with a free booking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 adults 2 children 1 room</span>
+          </div>
+        </div>
       </div>
     </div>
   );
