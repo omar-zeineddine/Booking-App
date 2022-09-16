b704b0749821fa57e41999a52018f8ea20d39e10
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-16 22:25:14 +0300

    feat - add option counter container

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -102,21 +102,27 @@ const Header = () => {
             <div className="options">
               <div className="optionItem">
                 <span className="optionText">Adult</span>
-                <button className="optionCounterButton">-</button>
-                <span className="optionCounterNumber">1</span>
-                <button className="optionCounterButton">+</button>
+                <div className="optionCounter">
+                  <button className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">1</span>
+                  <button className="optionCounterButton">+</button>
+                </div>
               </div>
               <div className="optionItem">
                 <span className="optionText">Children</span>
-                <button className="optionCounterButton">-</button>
-                <span className="optionCounterNumber">0</span>
-                <button className="optionCounterButton">+</button>
+                <div className="optionCounter">
+                  <button className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">0</span>
+                  <button className="optionCounterButton">+</button>
+                </div>
               </div>
               <div className="optionItem">
                 <span className="optionText">Room</span>
-                <button className="optionCounterButton">-</button>
-                <span className="optionCounterNumber">1</span>
-                <button className="optionCounterButton">+</button>
+                <div className="optionCounter">
+                  <button className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">1</span>
+                  <button className="optionCounterButton">+</button>
+                </div>
               </div>
             </div>
           </div>
