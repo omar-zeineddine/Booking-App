366956e742b2e136621c8c819dabeefcc382debd
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-16 20:25:57 +0300

    style - add header styles

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,31 +1,38 @@
 import React from "react";
 import "./header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBed, faPlane, faCar, faTaxi } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="headerList">
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Stays</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faPlane} />
-          <span>Flights</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faCar} />
-          <span>Car rentals</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Attractions</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faTaxi} />
-          <span>taxis</span>
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>taxis</span>
+          </div>
         </div>
       </div>
     </div>
