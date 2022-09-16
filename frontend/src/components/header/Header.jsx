953b149cb27cec112ca4f28f9369ecd5bee8bf36
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-16 20:19:40 +0300

    feat - add font awesome icons to header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import "./header.css"
+import React from "react";
+import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBed, faPlane, faCar, faTaxi } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <div className='header'>Header</div>
-  )
-}
+    <div className="header">
+      <div className="headerList">
+        <div className="headerListItem">
+          <FontAwesomeIcon icon={faBed} />
+          <span>Stays</span>
+        </div>
+        <div className="headerListItem">
+          <FontAwesomeIcon icon={faPlane} />
+          <span>Flights</span>
+        </div>
+        <div className="headerListItem">
+          <FontAwesomeIcon icon={faCar} />
+          <span>Car rentals</span>
+        </div>
+        <div className="headerListItem">
+          <FontAwesomeIcon icon={faBed} />
+          <span>Attractions</span>
+        </div>
+        <div className="headerListItem">
+          <FontAwesomeIcon icon={faTaxi} />
+          <span>taxis</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header   
+export default Header;
