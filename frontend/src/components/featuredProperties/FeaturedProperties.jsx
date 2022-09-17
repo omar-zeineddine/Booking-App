e05de1ccb6fe4058ca8a9004e749295f66b67099
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-17 12:24:38 +0300

    feat - add featured properties component

---
+++ frontend/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./featuredProperties.css";
+
+const FeaturedProperties = () => {
+  return <div>FeaturedProperties</div>;
+};
+
+export default FeaturedProperties;
