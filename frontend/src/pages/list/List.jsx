5f2541514da52d4135defd61d78921adce769bfe
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-17 13:48:11 +0300

    feat - get states from location

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import "./list.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import { useLocation } from "react-router-dom";
 
 const List = () => {
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [options, setOptions] = useState(location.state.options);
+
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input type="text" />
+            </div>
+            <div className="lsItem">
+              <label>Check-in date</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   );
 };
