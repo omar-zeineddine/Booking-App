c158e9e540d4302cd7a75252746937e6ddb73606
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-18 20:19:50 +0300

    backend - add dev packages

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 const app = express ();
 
-app.listen(5000, () => {
-  console.log("connected to backend")
-})
+// port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
