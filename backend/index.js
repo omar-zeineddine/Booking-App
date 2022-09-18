ad5482bbbfb118c6437a1fdd2f7deecb51ed143a
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-18 23:21:29 +0300

    backend - connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
-import express from "express"
-const app = express ();
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
 
 // port
 const PORT = process.env.PORT || 5000;
