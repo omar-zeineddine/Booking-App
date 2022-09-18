2d28cb75e01b8c91d8aa3e2f520d7c616fb7c0c3
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-18 23:28:06 +0300

    backend - add mongoose and establish db connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
 dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
 
 // port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  connect();
   console.log(`Server is running on port: ${PORT}`);
 });
