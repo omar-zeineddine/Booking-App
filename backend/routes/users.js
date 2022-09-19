5288c8263d526ccad4f99a95d8171deafbab4ad7
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-19 18:40:45 +0300

    backend - add verification functions to routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.get("/:id", getUser);
-router.get("/", getUsers);
+router.put("/:id", verifyUser, updateUser);
+router.delete("/:id", verifyUser, deleteUser);
+router.get("/:id", verifyUser, getUser);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
