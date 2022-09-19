c2211aa8b81a2de50e415de4ef7fb35c1e4c045e
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-19 13:19:57 +0300

    backend - add register and login functions

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -15,6 +16,24 @@ export const register = async (req, res, next) => {
 
     await newUser.save();
     res.status(201).send("user created");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    // check user in db
+    if (!user) return next(createError(404, "user not found"));
+
+    // compare against pass in db
+    const isPass = await bcrypt.compare(req.body.password, user.password);
+
+    // in case pass is wrong
+    if (!isPass) return next(createError(400, "Incorrect credentials"));
+
+    res.status(200).json(user);
   } catch (err) {
     next(err);
   }
