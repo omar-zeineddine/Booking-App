84731783ab1daa757a7d9ec541cc3c017b7ef3c1
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-19 19:00:34 +0300

    fix - adjust update hotel function

--- backend/controllers/hotel.js
+++ backend/controllers/hotel.js
@@ -12,11 +12,13 @@ export const createHotel = async (req, res, next) => {
 };
 
 export const updateHotel = async (req, res, next) => {
-  const newHotel = new Hotel(req.body);
-
   try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
   } catch (err) {
     next(err);
   }
@@ -25,7 +27,7 @@ export const updateHotel = async (req, res, next) => {
 export const deleteHotel = async (req, res, next) => {
   try {
     await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("hotel has been deleted");
+    res.status(200).json("Hotel has been deleted");
   } catch (err) {
     next(err);
   }
