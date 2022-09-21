d149d521985e452f12a6d159ecf7bc1ff736d77f
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-21 20:48:56 +0300

    feat - add navbar link

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import './navbar.css'
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-      <div className="navbar">
-        <div className="navContainer">
-            <span className="logo">booking</span>
-      <div className="navItems">
-        <button className="navButton">Register</button>
-        <button className="navButton">Login</button>
-      </div>
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" className="linkLogo">
+          <span className="logo">booking</span>
+        </Link>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
