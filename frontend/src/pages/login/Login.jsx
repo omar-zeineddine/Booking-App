069a393c9877bb786230de2120e1e63351862edd
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-22 21:55:30 +0300

    feat - add login route

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,5 +1,7 @@
 import "./login.css";
 import { AuthContext } from "../../context/AuthContext";
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
 
 const Login = () => {
   const [creds, setCreds] = useState({
@@ -7,7 +9,28 @@ const Login = () => {
     password: undefined,
   });
 
-  const { loading, error, dispatch } = useContext(AuthContext);
+  const { user, loading, error, dispatch } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setCreds((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/v1/auth/login",
+        creds
+      );
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+  };
+
+  console.log(user);
+
   return (
     <div className="login">
       <div className="lContainer">
@@ -25,7 +48,9 @@ const Login = () => {
           onChange={handleChange}
           className="lInupt"
         />
-        <button className="lButton">Login</button>
+        <button onClick={handleLogin} className="lButton">
+          Login
+        </button>
         {error && <span>{error.message}</span>}
       </div>
     </div>
