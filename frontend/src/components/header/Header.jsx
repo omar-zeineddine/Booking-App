97c2bc97bb2eb0b401ec163495e8dc1c4a363a9a
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-22 19:05:47 +0300

    fix - adjust date state name

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -18,12 +18,14 @@ import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 
 import { format } from "date-fns";
+import { useContext } from "react";
+import { SearchContext } from "../../context/SearchContext";
 
 const Header = ({ type }) => {
   // calendar states
   const [openDate, setOpenDate] = useState(false);
   // date range states
-  const [date, setDate] = useState([
+  const [dates, setDates] = useState([
     {
       startDate: new Date(),
       endDate: new Date(),
@@ -52,8 +54,11 @@ const Header = ({ type }) => {
 
   // handle search function
   const navigate = useNavigate();
+
+  const { dispatch } = useContext(SearchContext);
   const handleSearch = () => {
-    navigate("/hotels", { state: { destination, date, options } });
+    dispatch({ type: "NEW_SEARCH", payload: { destination, dates, options } });
+    navigate("/hotels", { state: { destination, dates, options } });
   };
 
   return (
@@ -110,16 +115,16 @@ const Header = ({ type }) => {
                 <span
                   onClick={() => setOpenDate(!openDate)}
                   className="headerSearchText"
-                >{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(
-                  date[0].endDate,
+                >{`${format(dates[0].startDate, "dd/MM/yyyy")} to ${format(
+                  dates[0].endDate,
                   "dd/MM/yyyy"
                 )}`}</span>
                 {openDate && (
                   <DateRange
                     editableDateInputs={true}
-                    onChange={(item) => setDate([item.selection])}
+                    onChange={(item) => setDates([item.selection])}
                     moveRangeOnFirstSelection={false}
-                    ranges={date}
+                    ranges={dates}
                     className="date"
                     minDate={new Date()}
                   />
