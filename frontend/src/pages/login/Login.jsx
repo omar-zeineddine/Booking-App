1d88b9e22ca3869d81354f5829af4ad00c845bfd
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-23 11:51:05 +0300

    style - add login page style

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,35 +1,37 @@
-import "./login.css";
-import { AuthContext } from "../../context/AuthContext";
-import { useState, useEffect, useContext } from "react";
 import axios from "axios";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import "./login.css";
 
 const Login = () => {
-  const [creds, setCreds] = useState({
+  const [credentials, setCredentials] = useState({
     username: undefined,
     password: undefined,
   });
 
-  const { user, loading, error, dispatch } = useContext(AuthContext);
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setCreds((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
 
-  const handleLogin = async (e) => {
+  const handleClick = async (e) => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
       const res = await axios.post(
         "http://localhost:5000/api/v1/auth/login",
-        creds
+        credentials
       );
-      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+      navigate("/");
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
   };
-
-  console.log(user);
 
   return (
     <div className="login">
@@ -39,16 +41,16 @@ const Login = () => {
           placeholder="username"
           id="username"
           onChange={handleChange}
-          className="lInupt"
+          className="lInput"
         />
         <input
           type="password"
-          placeholder="passowrd"
-          id="passowrd"
+          placeholder="password"
+          id="password"
           onChange={handleChange}
-          className="lInupt"
+          className="lInput"
         />
-        <button onClick={handleLogin} className="lButton">
+        <button disabled={loading} onClick={handleClick} className="lButton">
           Login
         </button>
         {error && <span>{error.message}</span>}
