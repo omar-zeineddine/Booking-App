fa1f339762da05deb68698c1374fb82df3072af7
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-24 14:24:24 +0300

    fix - update user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,25 +1,25 @@
 import User from "../models/User.js";
 
 export const updateUser = async (req, res, next) => {
-  const newUser = new Hotel(req.body);
-
   try {
-    const savedUser = await newHotel.save();
-    res.status(200).json(savedUser);
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
   } catch (err) {
     next(err);
   }
 };
-
 export const deleteUser = async (req, res, next) => {
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.status(200).json("hotel has been deleted");
+    res.status(200).json("User has been deleted.");
   } catch (err) {
     next(err);
   }
 };
-
 export const getUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
@@ -28,7 +28,6 @@ export const getUser = async (req, res, next) => {
     next(err);
   }
 };
-
 export const getUsers = async (req, res, next) => {
   try {
     const users = await User.find();
