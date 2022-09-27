2bf6bdbf887d4c53624c4584db568cda462f2807
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-27 20:01:42 +0300

    feat - compare date timestamps

--- frontend/src/components/reserve/Reserve.jsx
+++ frontend/src/components/reserve/Reserve.jsx
@@ -3,12 +3,33 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import useFetch from "../../hooks/useFetch";
 import { useState } from "react";
+import { useContext } from "react";
+import { SearchContext } from "../../context/SearchContext";
 
 const Reserve = ({ setOpen, hotelId }) => {
   const [selectedRooms, setSelectedRooms] = useState([]);
   const { data, loading, error } = useFetch(
     `http://localhost:5000/api/v1/hotels/room/${hotelId}`
   );
+
+  // date functions
+  const { dates } = useContext(SearchContext);
+
+  const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    const date = new Date(start.getTime());
+
+    const dates = [];
+
+    while (date <= end) {
+      dates.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
+    }
+
+    return dates;
+  };
 
   const handleSelect = (e) => {
     const checked = e.target.checked;
@@ -20,7 +41,6 @@ const Reserve = ({ setOpen, hotelId }) => {
     );
   };
 
-  console.log(selectedRooms);
   return (
     <div className="reserve">
       <div className="rContainer">
@@ -48,12 +68,14 @@ const Reserve = ({ setOpen, hotelId }) => {
                     type="checkbox"
                     value={roomNumber._id}
                     onChange={handleSelect}
+                    // disabled={!isAvailable(roomNumber)}
                   />
                 </div>
               ))}
             </div>
           </div>
         ))}
+        <button className="rButton">Reserve Now</button>
       </div>
     </div>
   );
