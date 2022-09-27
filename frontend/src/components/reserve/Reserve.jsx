ad0f50bde2bbc0f65ca8140fab1838346b0fe30e
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-27 19:11:21 +0300

    feat - add font icons to modal

--- frontend/src/components/reserve/Reserve.jsx
+++ frontend/src/components/reserve/Reserve.jsx
@@ -1,7 +1,19 @@
 import "./reserve.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Reserve = ({ setOpen, hotelId }) => {
-  return <div className="reserve">Reserve</div>;
+  return (
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="rclose"
+          onClick={() => setOpen(false)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Reserve;
